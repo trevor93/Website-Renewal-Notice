0d02ac26d84030be56516be0d701755803a2304c
trevor93 <salminabdalla93@gmail.com>
2025-10-31 14:32:30 +0300

    Updated siteActivation.js

--- public/utils/siteActivation.js
+++ public/utils/siteActivation.js
@@ -19,7 +19,7 @@ async function checkSiteActivation(domainName) {
       {
         headers: {
           'apikey': SUPABASE_ANON_KEY,
-          'Accept': 'application/json',
+          Authorization: `Bearer ${SUPABASE_ANON_KEY}`,
         },
       }
     );
